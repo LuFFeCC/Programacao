98a1251c1560ed1252cfbe40c6b82e0a384d4543
Fernando Costa <lfernando.costaa@gmail.com>
2022-02-28 10:51:12 -0300

    Update principal.py
    Arrumação do código

--- Python/PROJETOS/principal.py
+++ Python/PROJETOS/principal.py
@@ -8,21 +8,18 @@ def myping(host):         #FUNÇÃO PARA PINGAR HOSTS E VERIFICAR SE ESTÃO ATIV
     resposta_ping = (os.system('ping -n 1 ' +host ))
     return resposta_ping
 
-def ssh_connect(command):   #FUNÇÃO PARA SE CONECTAR AO LINUX POR SSH
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=adress, username=username, password=password)
-    stdin, stdout, stderr, = ssh.exec_command(command)
-    stdin.close()
-    resposta_ssh = stdout.readlines()
-    return resposta_ssh
-
-def windows(adress):     #FUNÇÃO PARA EXECUTAR A CONEXÃO E COMANDOS NAS MAQUINAS WINDOWS
+def windows_check(adress):     #FUNÇÃO PARA EXECUTAR A CONEXÃO E VERIFICAR SE A MAQUINA É WINDOWS OU LINUX
     parametro=0
-    parametro = 'paexec.exe \\\\'+adress+' -u MIXHC\\fernando -p @mix080117 wmic'
-    c1 = 'paexec.exe \\\\'+adress+' -u MIXHC\\fernando -p @mix080117 product get name'
+    parametro = 'paexec.exe \\\\'+adress+' -u MIXHC\\fernando -p @mix080117 whoami' 
     resposta_windows = os.system(parametro)
-    resposta_windows = os.system(c1)
+    return resposta_windows
+
+def windows_list(adress):     #FUNÇÃO PARA EXECUTAR A CONEXÃO E COMANDOS NAS MAQUINAS WINDOWS
+    parametro=0
+    #LISTANDO PROGRAMAS INSTALADOS NAS MAQUINAS E COLOCANDO DENTRO DE UM ARQUIVO NA MAQUINA CENTRAL
+    parametro = 'paexec.exe \\\\'+adress+' -u MIXHC\\fernando -p @mix080117 copy NUL D:/REDE/'+ip+'/programas_instalados.txt'
+    parametro = 'paexec.exe \\\\'+adress+' -u MIXHC\\fernando -p @mix080117 wmic product get name > D:/REDE/'+ip+'/programas_instalados.txt' 
+    resposta_windows = os.system(parametro)
     return resposta_windows
 
 ipsvalidos = []   # LISTA PARA GUARDAR OS IPS VALIDOS, E COLOCAR NO ARQUIVO IPS_VALIDOS.TXT
@@ -35,18 +32,22 @@ password =  "teste123!!" #input(print("DIGITE A SENHA: "))
 print("Digite a sua rede Ex: 192.168.0")
 rede = input("REDE: ")
 
-for i in range(0,1):
-    ip = rede+'.'+str(46)
+for i in range(0,255):
+    ip = rede+'.'+str(i)
     ipsvalidos = myping(ip)
     
     
     if ipsvalidos == 0 :
+
+        os.system('mkdir D:\\REDE\\'+ ip)
         
-        resposta_funcao = windows(str(ip))
+        resposta_funcao = windows_check(str(ip))
         if resposta_funcao == 0:
             maquina = "MAQUINA WINDOWS:"
             ip_valido = open('ipswindows.txt','a')
             ip_valido.write(str(ip)+'\n')
+            executado=windows_list(str(ip))
+
         else :
             maquina = "Linux"
             ip_valido = open('ipslinux.txt','a')
@@ -55,3 +56,4 @@ for i in range(0,1):
         ip_valido = open('ips_validos.txt','a')
         ip_valido.write(str(ip)+'\n')
         ip_valido.write(str(maquina)+'\n\n')
+
